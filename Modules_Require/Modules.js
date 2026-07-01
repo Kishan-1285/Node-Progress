46eb9a5e518953d7cfec823854c20bf679094af8
kishan-1285 <kishanragunathan26@gmail.com>
2026-07-01 17:52:05 +0530

    modules and require

--- Modules_Require/Modules.js
+++ Modules_Require/Modules.js
@@ -1,3 +1,7 @@
 const data = require('./data');
 
 console.log(data);
+
+const os = require('os');
+console.log(os);
+console.log(os.homedir());
--- Modules_Require/data.js
+++ Modules_Require/data.js
@@ -1,6 +1,6 @@
 const name = ["asdf","ashwin","kishan"];
 const ages = [23,23,43,32,13,4,5,5,6]
-console.log("makkuti ")
+
 
 
 module.exports = {
