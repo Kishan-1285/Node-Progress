bafbc78ef2e6b108bc8fb2cfb6e3e33d81d5911d
kishan-1285 <kishanragunathan26@gmail.com>
2026-07-01 16:13:05 +0530

    modules and require

---
+++ Modules_Require/data.js
@@ -0,0 +1,16 @@
+const name = ["asdf","ashwin","kishan"];
+const ages = [23,23,43,32,13,4,5,5,6]
+console.log("makkuti ")
+
+
+module.exports = {
+    peyar:name,
+    vayathu: ages
+};
+
+// or 
+
+module.exports={
+    peru : name,
+    vayasu : ages
+}
