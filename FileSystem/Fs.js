0d39838abacc611907f476a1247e1bed2b8796d1
kishan-1285 <kishanragunathan26@gmail.com>
2026-07-01 18:43:09 +0530

    File System

---
+++ FileSystem/Fs.js
@@ -0,0 +1,26 @@
+const fs = require('fs');  //runs the file
+
+
+
+// checks if the document exits 
+console.log(fs.existsSync('./document'))   // it is a synchronous function
+
+// checks if the document does not exits if not then the statement will be executed
+if( !fs.existsSync('./document')){                                                
+
+fs.mkdir('./documents',(err)=>{   // mkdir -- used to create files or folder
+    if(err){
+        console.log(err.message);
+    }else{
+        console.log("file Created")
+    }
+})
+}
+
+fs.writeFile('./documents/file.txt','Hello bruhhh',(err)=>{   // used to write values to the desired location
+     if(err){
+        console.log(err.message);
+    }else{
+        console.log('file Written');
+    }
+});
